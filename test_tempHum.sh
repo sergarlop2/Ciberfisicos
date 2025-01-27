a5b64639f245334f178f964b6cea45b9ff158f8a
Sergio García López <sergarlop2@alum.us.es>
2025-01-27 11:31:22 +0100

    Actualizar test temperatura y humedad

--- test_tempHum.sh
+++ test_tempHum.sh
@@ -1,8 +1,31 @@
 #!/bin/bash
 
-# Envia 5 mensajes de temperatura y humedad
-mosquitto_pub -h broker.hivemq.com -p 1883 -t ciberfisicos/temp_hum -m '{"temperatura": 22.5, "humedad": 55.0, "timestamp": "2025-01-16T20:05:00Z"}'
-mosquitto_pub -h broker.hivemq.com -p 1883 -t ciberfisicos/temp_hum -m '{"temperatura": 23.0, "humedad": 56.0, "timestamp": "2025-01-16T20:10:00Z"}'
-mosquitto_pub -h broker.hivemq.com -p 1883 -t ciberfisicos/temp_hum -m '{"temperatura": 21.8, "humedad": 54.5, "timestamp": "2025-01-16T20:15:00Z"}'
-mosquitto_pub -h broker.hivemq.com -p 1883 -t ciberfisicos/temp_hum -m '{"temperatura": 22.2, "humedad": 53.8, "timestamp": "2025-01-16T20:20:00Z"}'
-mosquitto_pub -h broker.hivemq.com -p 1883 -t ciberfisicos/temp_hum -m '{"temperatura": 21.5, "humedad": 52.3, "timestamp": "2025-01-16T20:25:00Z"}'
+# Configuración MQTT
+BROKER="broker.hivemq.com"
+PORT=1883
+TOPIC="SCF/sejuja/data/tempHum"
+
+# Número de mensajes a enviar
+MESSAGES=5
+
+# Datos
+TEMP_VALUES=("22.5" "23.0" "21.8" "22.2" "21.5")
+HUM_VALUES=("55.0" "56.0" "54.5" "53.8" "52.3")
+
+# Publicar los mensajes
+for ((i=0; i<$MESSAGES; i++)); do
+  # Obtener el timestamp actual
+  TIMESTAMP=$(date -u +"%d-%m-%yT%H:%M:%S.000")
+
+  # Crear el mensaje JSON
+  JSON_DATA="{\"temperatura\": ${TEMP_VALUES[$i]}, \"humedad\": ${HUM_VALUES[$i]}, \"timestamp\": \"$TIMESTAMP\"}"
+
+  # Publicar el mensaje
+  mosquitto_pub -h "$BROKER" -p "$PORT" -t "$TOPIC" -m "$JSON_DATA"
+  
+  # Mostrar el mensaje enviado
+  echo "Mensaje enviado: $JSON_DATA"
+  
+  # Esperar un intervalo entre los mensajes
+  sleep 1
+done
